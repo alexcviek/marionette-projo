011191c03d4a76da138c491e3718f5916539e375
Alex Cwiek <a.i.cwiek@gmail.com>
2017-09-08 15:58:11 +0100

    show button added

--- assets/js/apps/contacts/list/list_view.js
+++ assets/js/apps/contacts/list/list_view.js
@@ -1,7 +1,31 @@
 ContactManager.module('ContactsApp.List', function(List, ContactManager, Backbone, Marionette, $, _){
   List.Contact = Marionette.ItemView.extend({
     tagName: 'tr',
-    template: '#contact-list-item'
+    template: '#contact-list-item',
+    events: {
+      'click': 'highlightName',
+      'click td a.js-show': 'showClicked',
+      'click button.js-delete': 'deleteClicked'
+    },
+    highlightName: function(e){
+      e.preventDefault();
+      this.$el.toggleClass('warning');
+    },
+    showClicked: function(e){
+      e.preventDefault();
+      e.stopPropagation();
+      this.trigger('contact:show', this.model);
+    },
+    deleteClicked: function(e){
+      e.stopPropagation();
+      this.trigger('contact:delete', this.model);
+    },
+    remove: function(){
+      var self = this;
+      this.$el.fadeOut(function(){
+        Marionette.ItemView.prototype.remove.call(self);
+      });
+    }
   });
 
   List.Contacts = Marionette.CompositeView.extend({
@@ -9,6 +33,11 @@ ContactManager.module('ContactsApp.List', function(List, ContactManager, Backbon
     className: 'table table-hover',
     template: '#contact-list',
     childView: List.Contact,
-    childViewContainer: 'tbody'
+    childViewContainer: 'tbody',
+    onChildviewContactDelete: function(){
+      this.$el.fadeOut(1000, function(){
+        $(this).fadeIn(1000);
+      });
+    }
   });
 });
