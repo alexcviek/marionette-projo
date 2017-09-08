0812be2305df53d034882f15a85df192d45fbf3d
Alex Cwiek <a.i.cwiek@gmail.com>
2017-09-08 12:49:03 +0100

    split the app into modules

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,17 @@
-console.log('o hai');
+var ContactManager = new Marionette.Application();
+
+ContactManager.on('before:start', function(){
+  var RegionContainer = Marionette.LayoutView.extend({
+    el: '#app-container',
+
+    regions: {
+      main: '#main-region'
+    }
+  });
+
+  ContactManager.regions = new RegionContainer();
+});
+
+ContactManager.on('start', function(){
+  ContactManager.ContactsApp.List.Controller.listContacts();
+});
