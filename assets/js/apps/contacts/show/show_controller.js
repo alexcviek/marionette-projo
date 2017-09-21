3d18ff8d585a3d81af58dfef412ea7c9529803fb
Alex Cwiek <a.i.cwiek@gmail.com>
2017-09-21 12:50:08 +0100

    loading delay visual

--- assets/js/apps/contacts/show/show_controller.js
+++ assets/js/apps/contacts/show/show_controller.js
@@ -1,17 +1,22 @@
 ContactManager.module('ContactsApp.Show', function(Show, ContactManager, Backbone, Marionette, $, _){
   Show.Controller = {
     showContact: function(id){
-      var contact = ContactManager.request('contact:entity', id);
-      var contactView;
-      if(contact !== undefined){
-        contactView = new Show.Contact({
-          model: contact
-        });
-      }else{
-        contactView = new Show.MissingContact();
-      }
+      var loadingView = new ContactManager.Common.Views.Loading();
+      ContactManager.regions.main.show(loadingView);
+      
+      var fetchingContact = ContactManager.request('contact:entity', id);
+      $.when(fetchingContact).done(function(contact){
+        var contactView;
+        if(contact !== undefined){
+          contactView = new Show.Contact({
+            model: contact
+          });
+        }else{
+          contactView = new Show.MissingContact();
+        }
 
-      ContactManager.regions.main.show(contactView);
+        ContactManager.regions.main.show(contactView);
+      });
     }
-  }
+  };
 });
